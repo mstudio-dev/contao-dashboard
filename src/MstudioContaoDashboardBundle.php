e13791d0c55f6c2b8d4bf9d40e02d120f4d0c132
Markus Schnagl <mail@mstudio.de>
2026-01-19 13:46:43 +0100

    Fix: getPath() wieder hinzugefügt für Asset-Installation
    - Ohne getPath() erkennt Symfony den public/-Ordner nicht
    - Assets werden nicht installiert -> Icons nicht verfügbar
    - getPath() gibt Bundle-Root zurück für korrekten public/-Pfad
    - Behebt: Tiles erscheinen und verschwinden sofort

--- src/MstudioContaoDashboardBundle.php
+++ src/MstudioContaoDashboardBundle.php
@@ -8,5 +8,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MstudioContaoDashboardBundle extends Bundle
 {
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
 
